4350fe39ccd63a44c05f641a4c7fca62add1cb0d
Alysa Barbarisi <baal02@cox.net>
2018-06-12 21:22:05 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do 
+    "My name is "
+  end 
+  get '/'do
+    "My hometown is"
+  end 
+  get '/' do
+    "My favorite song is "
+  end 
 end
